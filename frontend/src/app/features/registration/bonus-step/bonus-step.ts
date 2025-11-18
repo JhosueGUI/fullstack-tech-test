2a213de3698ebcf079dfaddf9f1e3aa92826ff83
JhosueGUI <1458823@senati.pe>
2025-11-18 01:25:28 +0100

    front

--- frontend/src/app/features/registration/bonus-step/bonus-step.ts
+++ frontend/src/app/features/registration/bonus-step/bonus-step.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+// src/app/features/registration/bonus-step/bonus-step.ts
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bonus-step',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './bonus-step.html',
-  styleUrl: './bonus-step.css',
+  styleUrls: ['./bonus-step.css']
 })
 export class BonusStep {
+  @Output() bonusSelected = new EventEmitter<string>();
 
+  // Llamado al hacer click en una opción
+  select(bono: string) {
+    this.bonusSelected.emit(bono);
+  }
 }
--- frontend/src/app/features/registration/registration.page.ts
+++ frontend/src/app/features/registration/registration.page.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BonusStep } from './bonus-step/bonus-step';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [
+    CommonModule,
+    BonusStep
+  ],
   templateUrl: './registration.page.html',
   styleUrl: './registration.page.css',
 })
 export class RegistrationPage {
+currentStep: 'bonus' | 'form' = 'bonus';
+  selectedBonus: string | null = null;
 
+  goToForm(bono: string) {
+    this.selectedBonus = bono;
+    this.currentStep = 'form';
+  }
 }
