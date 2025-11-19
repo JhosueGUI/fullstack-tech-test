09afe991ac75a507fb92bfeaf3b854455477b864
JhosueGUI <1458823@gmail.com>
2025-11-19 00:05:36 -0500

    s

--- frontend/src/app/features/registration/register-step/register-step.ts
+++ frontend/src/app/features/registration/register-step/register-step.ts
@@ -1,8 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 interface Food{
   value: string;
@@ -10,7 +8,7 @@ interface Food{
 }
 @Component({
   selector: 'app-register-step',
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
+  imports: [ FormsModule],
   encapsulation: ViewEncapsulation.None,
   templateUrl: './register-step.html',
   styleUrl: './register-step.css',
